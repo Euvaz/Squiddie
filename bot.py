a89c841ff111dd411a22c27c6bc88f3ec574e301
euvaz <euvaz@protonmail.ch>
2020-07-31 06:19:52 -0700

    Adding Cool-Down Functionality

--- bot.py
+++ bot.py
@@ -1,18 +1,29 @@
 import discord
 from discord.ext import commands
+import time as t
 
 client = commands.Bot(command_prefix='>')
 
 
 @client.event
 async def on_ready():
+    await client.change_presence(status=discord.Status.online, activity=discord.Game('with FBI crime statistics'))
     print(f'{client.user} is ready')
 
 
 @client.command()
-async def nigger(ctx):
-    await ctx.send('nigger')
+async def kick(ctx):
+    pass
 
 
-client.run('NzM4MzI1NDgzOTg0Mzg4MTU3.XyKRMA.70RN0HbTynGy0e964CmAw3BhijU')
+@client.command()
+async def ban(ctx):
+    pass
 
+
+@client.command()
+async def mute(ctx):
+    pass
+
+
+client.run('NzM4MzI1NDgzOTg0Mzg4MTU3.XyKRMA.WN99uZQVN0cyYSq8RwnQz7oIuCI')
