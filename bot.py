7922468a7b80d4d48325b058e14aa411f449fa0a
euvaz <euvaz@protonmail.ch>
2020-07-31 16:35:31 -0700

    Optimized "pp" command; Added "8ball" command

--- bot.py
+++ bot.py
@@ -28,27 +28,46 @@ async def mute(ctx):
 
 
 @commands.cooldown(1, 5, commands.BucketType.user)
-@client.command()
+@client.command(name='pp')
 async def pp(ctx):
-    i = random.randint(1, 10)
+    length = random.choice(['=',
+                            '==',
+                            '===',
+                            '====',
+                            '=====',
+                            '======',
+                            '=======',
+                            '========',
+                            '=========',
+                            '==========',
+                            '===========',
+                            '============'])
 
-    def pp_length(x):
-        length = {
-                1: '=',
-                2: '==',
-                3: '===',
-                4: '====',
-                5: '=====',
-                6: '======',
-                7: '=======',
-                8: '========',
-                9: '=========',
-                10: '==========',
-                11: '===========',
-                12: '============'
-        }
-        return length.get(x, "Invalid Length")
+    await ctx.send('8' + length + 'D')
 
-    await ctx.send('8' + pp_length(i) + 'D')
+
+@commands.cooldown(1, 5, commands.BucketType.user)
+@client.command(name='8ball')
+async def _ball(ctx):
+    await ctx.send(random.choice(['As I see it, yes.',
+                                  'Ask again later.',
+                                  'Better not tell you now.',
+                                  'Cannot predict now.',
+                                  'Concentrate and ask again.',
+                                  'Don’t count on it.',
+                                  'It is certain.',
+                                  'It is decidedly so.',
+                                  'Most likely.',
+                                  'My reply is no.',
+                                  'My sources say no.',
+                                  'Outlook not so good.',
+                                  'Outlook good.',
+                                  'Reply hazy, try again.',
+                                  'Signs point to yes.',
+                                  'Very doubtful.',
+                                  'Without a doubt.',
+                                  'Yes.',
+                                  'Yes – definitely.',
+                                  'You may rely on it.']))
 
 client.run('NzM4MzI1NDgzOTg0Mzg4MTU3.XyKRMA.WN99uZQVN0cyYSq8RwnQz7oIuCI')
