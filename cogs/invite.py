3917536fc43e2eaf616360efc45f7289614acfac
euvaz <euvaz@protonmail.ch>
2021-12-19 08:10:55 -0800

    Removed useless parameters

--- cogs/invite.py
+++ cogs/invite.py
@@ -13,7 +13,7 @@ class Invite(commands.Cog):
 
     # Invite command
     @commands.command(name="invite")
-    async def invite(self, ctx, *, arg):
+    async def invite(self, ctx):
         inviteChannelID = 822606443722309642
         inviteURL = await ctx.guild.get_channel(inviteChannelID).create_invite()
         await ctx.send(f'Here is the invite link!: {inviteURL}')
